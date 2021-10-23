a6515d48428252910264a4185daf6de55b170a12
josousa82 <mfgas2@ou.ac.uk>
2021-10-23 02:27:17 +0100

    refactored to multi module project

---
+++ mvc-rest-app/src/main/java/com/training/rest/springrestproject/repositories/CustomerRepository.java
@@ -0,0 +1,15 @@
+package com.training.rest.springrestproject.repositories;
+
+
+
+import com.training.rest.springrestproject.domain.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    Optional<Customer> findByFirstName(String name);
+}
